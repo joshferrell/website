0bbfc81f60b92aab41269088ba3572b74e924be2
Josh Ferrell <josh@joshferrell.me>
2023-02-21 23:23:19 -0800

    fix: spelling errors on about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -24,13 +24,13 @@ const About = () => (
         I am a software engineer who is an expert at building scalable software
         solutions from startups and large enterprises. I have built design
         systems for GoodLeap, the largest solar financer in the US and
-        Confluent, a publicly funded company building event driven architecture.
+        Confluent, a publicly funded company building event-driven architecture.
       </Box>
       <p>
         Throughout my career, I have worked with a wide range of technologies
         and platforms including: Javascript, React, GIS software, image
         rendering, and event based architecture. I have a deep understanding of
-        software development methodologies, including Agile and Scrume, and have
+        software development methodologies, including Agile and Scrum, and have
         led large-scale development projects from concept to delivery.
       </p>
 
@@ -50,10 +50,10 @@ const About = () => (
       </aside>
 
       <p>
-        In addition to my technical skills, I posses excellent communication and
-        leadership skills. I have a proven ability to work collaboratively with
-        cross-function teams, and I am adept at managing and mentoring junior
-        developers.
+        In addition to my technical skills, I possess excellent communication
+        and leadership skills. I have a proven ability to work collaboratively
+        with cross-function teams, and I am adept at managing and mentoring
+        junior developers.
       </p>
       <p>
         If you&apos;re interested in learning more about my experience or
