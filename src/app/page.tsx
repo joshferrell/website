cb6879e716371f85aac6e16b53899b0f2c25503b
Josh Ferrell <josh@joshferrell.me>
2023-03-01 16:36:26 -0800

    fix: home page margin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,12 @@ import { CardLink, Link } from '~/components/link';
 import profilePhoto from '../../public/josh-profile.webp';
 
 const Home = () => (
-  <main id="skip" role="main" className={styles.main}>
+  <main
+    id="skip"
+    role="main"
+    className={styles.main}
+    style={{ marginBottom: '2rem' }}
+  >
     <div className={styles.heading}>
       <Box as="h1" variant="title">
         Hey, I&apos;m&nbsp;
