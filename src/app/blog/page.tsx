14aa1918d880f70fd85b01116ecb91a5743d9b96
Josh Ferrell <josh@joshferrell.me>
2023-04-01 00:21:15 -0700

    chore: cleaned up bad posts

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,4 +1,4 @@
-import { allBlogs, allWorks } from 'contentlayer/generated';
+import { allBlogs } from 'contentlayer/generated';
 import { compareDesc } from 'date-fns';
 
 import { Container } from '~/components/container';
@@ -13,8 +13,6 @@ const Blog = () => {
     .filter((post) => post.published)
     .sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)));
 
-  const works = allWorks.filter((post) => post.published && post.showOnBlog);
-
   return (
     <Container title="Thoughts">
       {posts.map((post) => (
@@ -22,15 +20,6 @@ const Blog = () => {
           blurUrl={post.blurUrl}
           key={post.slug}
           url={`/blog/${post.slug}`}
-          title={post.title}
-          category={post.category}
-          image={{ source: post.image, alt: post.imageAlt }}
-        />
-      ))}
-      {works.map((post) => (
-        <BlogCard
-          key={post.slug}
-          url={`/work/${post.slug}`}
           title={post.title}
           category={post.category}
           image={{ source: post.image, alt: post.imageAlt }}
